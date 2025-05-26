83793b133715ef0ba597ec0913c28122cb3763cd
John Russell <russell@sonickroft.com>
2025-05-27 01:11:35 +0800

    feature/side-navbar-generic-implementation

---
+++ src/app/manager/side-navigation-menu/side-navigation-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navigation-menu',
+  standalone: false,
+  templateUrl: './side-navigation-menu.component.html',
+  styleUrl: './side-navigation-menu.component.css'
+})
+export class SideNavigationMenuComponent {
+
+}
